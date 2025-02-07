6c8b6f010ab1a2b540064d2b4609821c33a5cc51
Hanzala Khan <pythondeveloper360@gmail.com>
2025-02-08 03:05:53 +0500

    DUpdate to loan approval Application

--- Loan Approval/visualisation.py
+++ Loan Approval/visualisation.py
@@ -3,18 +3,33 @@ import plotly.express as px
 
 df = pd.read_csv('./datasets/loan_approval_dataset.csv')
 
-grouped = df.groupby([' loan_status',' self_employed'])[' loan_status'].size().reset_index(name = 'count')
+grouped_self_employed = df.groupby([' self_employed',' loan_status'])[' loan_status'].size().reset_index(name = 'count')
+grouped_no_of_dependents = df.groupby([' no_of_dependents',' loan_status'])[' loan_status'].size().reset_index(name = 'count')
 
-fig = px.bar(
-    grouped,x = ' loan_status',
+grouped_self_employed_fig = px.bar(
+    grouped_self_employed,
+    x = ' self_employed',
     y = 'count',
     color = ' loan_status',
     title='Loan Approval Status by Employment Status',
-    labels={"count":"Number of Applicants",' loan_status':'Loan Status'},
+    labels={"count":"Number of Applicants",' loan_status':'Loan Status',' self_employed':'Self Employed'},
     barmode='group',
 
     )
 
-    
-fig.write_html('first_figure.html', auto_open=True)
+
+grouped_no_of_dependents_fig = px.bar(
+    grouped_no_of_dependents,
+    x = ' no_of_dependents',
+    y = 'count',
+    color = ' loan_status',
+    title='Loan Approval Status by Number of dependents',
+    labels={"count":"Number of Dependents",' loan_status':'Loan Status',' no_of_dependents':'No of dependents'},
+    barmode='group',
+    )
+
+
+grouped_no_of_dependents_fig.write_html('./static/grouped_no_of_dependents.html')
+grouped_self_employed_fig.write_html('./static/grouped_self_employed.html')
+
 
